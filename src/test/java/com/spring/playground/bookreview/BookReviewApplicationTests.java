6ed343959e6eb844f5de687cb906cb80e5fda608
duck-ayoung <duck.ayoung@gmail.com>
2022-01-14 16:37:59 +0900

    Initial commit

---
+++ src/test/java/com/spring/playground/bookreview/BookReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.playground.bookreview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookReviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
